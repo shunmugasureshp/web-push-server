d47302255a8d08825e2cf9128b085e6bff5380ff
shunmugasureshp <ppshunmugasuresh@gmail.com>
2018-06-22 20:49:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -44,6 +44,23 @@ app.get('/status', function (req, res) {
 });
 
 app.get('/sublist', function (req, res) {
+    
+    const client = new Client({
+      connectionString: process.env.DATABASE_URL,
+      ssl: true,
+    });
+
+    client.connect();
+    
+    client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+      if (err) throw err;
+      for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+      }
+      client.end();
+    });
+    
+    
     res.send(JSON.stringify(subscribers));
 });
 
